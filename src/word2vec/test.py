a7266b9f3c4131834736918d5b37e20f252ccea4
DJ <donald.price077@topper.wku.edu>
2019-09-16 05:00:57 -0500

    code that generated the word2vec model

---
+++ src/word2vec/test.py
@@ -0,0 +1,10 @@
+from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import Word2Vec
+
+# TODO: figure out how many words there are in the corpus
+path = get_tmpfile("word2vec.model")
+
+model = Word2Vec(common_texts, size=100, window=5, min_count=1, workers=4)
+model.save("word2vec.model")
+
+print common_texts
