84df7704b2e77897ef37fd480f7c3fb89d6d9572
DJ <donald.price077@topper.wku.edu>
2019-09-16 05:01:44 -0500

    code to test loading of the doc2vec model

--- src/doc2vec/test_load.py
+++ src/doc2vec/test_load.py
@@ -1,5 +1,6 @@
 import gensim
 
-model = gensim.models.doc2vec.Doc2Vec.load("new_model.d2v")
-print model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires'])
+model = gensim.models.doc2vec.Doc2Vec.load("doc2vec/new_model.model")
+inferred_vector = model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires'])
 
+print model.docvecs.most_similar([inferred_vector], topn=3)
