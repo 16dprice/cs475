1dfcf826e152283b8566f0ed43e0fd09c336591d
DJ <donald.price077@topper.wku.edu>
2019-09-22 07:20:13 -0500

    saving training corpus to txt file

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from html_parsing.parsing_aggregator import ParsingAggregator
 start = time.time()
 
 parsing_aggregator = ParsingAggregator()
-parsing_aggregator.train_doc2vec_model()
-
-parsing_aggregator.save_doc2vec_model()
+parsing_aggregator.save_train_corpus("./src/html_parsing")
 
 end = time.time()
 
