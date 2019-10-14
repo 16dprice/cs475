7e83fb203a3d92f2eab8f1d1a5d427a8c49ad38e
DJ <donald.price077@topper.wku.edu>
2019-10-14 08:41:39 -0500

    change tsne generator to take into consideration that dance pdfs are added to the corpus. also coloring the text so it's more visible

--- generate_tsne_representations.py
+++ generate_tsne_representations.py
@@ -19,7 +19,7 @@ import sys
 early_exaggeration = float(sys.argv[1])  # default: 12.0
 random_state = int(sys.argv[2])  # default: None
 
-mtg_train_file = os.getcwd() + '/src/aggregate_train_corpus.txt'
+train_file = os.getcwd() + '/src/aggregate_train_corpus.txt'
 
 # yield produces a value that can only be iterated over once
 # that is, it's not stored in memory and is deallocated once it's iterated over
@@ -36,34 +36,48 @@ def read_corpus(fname, tokens_only=False):
                 yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
 
 
-train_corpus = list(read_corpus(mtg_train_file))
+train_corpus = list(read_corpus(train_file))
 model = gensim.models.doc2vec.Doc2Vec.load("./src/aggregate_model.model")
 
 doc_vectors = np.array(model.docvecs.vectors_docs)
 embedded_doc_vectors = TSNE(n_components=2, early_exaggeration=early_exaggeration, random_state=random_state).fit_transform(doc_vectors)
 
+# init the list so there's not a lot of appending when adding the labels
+# the label is hardcoded
 embedded_doc_vectors_with_words = [None for i in range(len(doc_vectors))]
 for doc_id in range(len(doc_vectors)):
-    label = "s"
-    if doc_id < 50:
+    # 0 - 49: mtg
+    # 50 - 68: sports
+    # 69+: bharatanatyam
+    if doc_id <= 49:
         label = "m"
+        color = dict(facecolor='blue', alpha=0.5)
+    elif 50 <= doc_id <= 68:
+        label = "s"
+        color = dict(facecolor='red', alpha=0.5)
+    else:
+        label = "b"
+        color = dict(facecolor='black', alpha=0.5)
 
-    embedded_doc_vectors_with_words[doc_id] = [(embedded_doc_vectors[doc_id, 0], embedded_doc_vectors[doc_id, 1]), label]
+    embedded_doc_vectors_with_words[doc_id] = [(embedded_doc_vectors[doc_id, 0], embedded_doc_vectors[doc_id, 1]), label, color]
 
-
+# find the bounds for the axes
 minX = min(embedded_doc_vectors[0:, 0])
 maxX = max(embedded_doc_vectors[0:, 0])
 
 minY = min(embedded_doc_vectors[0:, 1])
 maxY = max(embedded_doc_vectors[0:, 1])
 
+# define axes and label each point
 plt.axis([minX - 10, maxX + 10, minY - 10, maxY + 10])
-for point, label in embedded_doc_vectors_with_words:
-    plt.text(point[0], point[1], label)
+for point, label, color in embedded_doc_vectors_with_words:
+    plt.text(point[0], point[1], label, bbox=color)
 
 plt.title("Learning Exaggeration: {} -- Random State: {}".format(early_exaggeration, random_state))
 
-if not os.path.exists("./tsne_plot_outputs/random_state_{}".format(random_state)):
-    os.mkdir("./tsne_plot_outputs/random_state_{}".format(random_state))
+# sometimes the directory doesn't exist
+if not os.path.exists("./tsne_plot_outputs/sports_mtg_and_bharatanatyam/random_state_{}".format(random_state)):
+    os.mkdir("./tsne_plot_outputs/sports_mtg_and_bharatanatyam/random_state_{}".format(random_state))
 
-plt.savefig("./tsne_plot_outputs/random_state_{}/ee_{}_rs_{}.png".format(random_state, early_exaggeration, random_state))
+# file formats supported are png, pdf, and some mores. jpg and jpeg are not supported
+plt.savefig("./tsne_plot_outputs/sports_mtg_and_bharatanatyam/random_state_{}/ee_{}_rs_{}.png".format(random_state, early_exaggeration, random_state))
