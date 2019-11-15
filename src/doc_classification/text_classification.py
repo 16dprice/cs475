01206b3eceed7e55bd94ef90081104b2351f3ad5
DJ <donald.price077@topper.wku.edu>
2019-11-15 09:21:46 -0600

    smarter text classification script

--- src/doc_classification/text_classification.py
+++ src/doc_classification/text_classification.py
@@ -9,31 +9,52 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
-train_file = os.getcwd() + '/../aggregate_train_corpus.txt'
+def get_file_contents(path):
+    file_contents = []
+    with smart_open.open(path, encoding="UTF-8") as f:
+        for i, line in enumerate(f):
+            file_contents.append(line)
 
-file_contents = []
-with smart_open.open(train_file, encoding="UTF-8") as f:
-    for i, line in enumerate(f):
-        file_contents.append(line)
+    return file_contents
 
 
 ######################################### Splitting Data (70% train, 30% test) #########################################
 
 
+mtg_articles_file_contents = get_file_contents(os.getcwd() + '/../mtg_articles.txt')
+sports_articles_file_contents = get_file_contents(os.getcwd() + '/../sports_articles.txt')
+bharatanatyam_pdfs_file_contents = get_file_contents(os.getcwd() + '/../bharatanatyam_pdfs.txt')
+
+num_train_mtg_articles = round(0.7 * len(mtg_articles_file_contents))
+num_train_sports_articles = round(0.7 * len(sports_articles_file_contents))
+num_train_bharatanatyam_pdfs = round(0.7 * len(bharatanatyam_pdfs_file_contents))
+
+num_test_mtg_articles = len(mtg_articles_file_contents) - num_train_mtg_articles
+num_test_sports_articles = len(sports_articles_file_contents) - num_train_sports_articles
+num_test_bharatanatyam_pdfs = len(bharatanatyam_pdfs_file_contents) - num_train_bharatanatyam_pdfs
+
 # should be 35 mtg, 27 sports, 10 dance
-train_data = file_contents[:35] + file_contents[50:77] + file_contents[88:98]
+train_data = []
+train_data += mtg_articles_file_contents[:num_train_mtg_articles]
+train_data += sports_articles_file_contents[:num_train_sports_articles]
+train_data += bharatanatyam_pdfs_file_contents[:num_train_bharatanatyam_pdfs]
+
 # 15 mtg, 11 sports, 4 dance
-test_data = file_contents[35:50] + file_contents[64:75] + file_contents[98:]
+test_data = []
+test_data += mtg_articles_file_contents[num_train_mtg_articles:]
+test_data += sports_articles_file_contents[num_train_sports_articles:]
+test_data += bharatanatyam_pdfs_file_contents[num_train_bharatanatyam_pdfs:]
+
 
 train_targets = []
-for i in range(35): train_targets.append(0)
-for i in range(27): train_targets.append(1)
-for i in range(10): train_targets.append(2)
+for i in range(num_train_mtg_articles):       train_targets.append(0)
+for i in range(num_train_sports_articles):    train_targets.append(1)
+for i in range(num_train_bharatanatyam_pdfs): train_targets.append(2)
 
 test_targets = []
-for i in range(15): test_targets.append(0)
-for i in range(11): test_targets.append(1)
-for i in range(4 ): test_targets.append(2)
+for i in range(num_test_mtg_articles):        test_targets.append(0)
+for i in range(num_test_sports_articles):     test_targets.append(1)
+for i in range(num_test_bharatanatyam_pdfs):  test_targets.append(2)
 
 count_vect = CountVectorizer()
 tfidf_transformer = TfidfTransformer()
