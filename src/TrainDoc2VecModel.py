8120672dce7d8c8d47fad57f7a8f03b23b4f6d39
DJ <djpric91@gmail.com>
2020-06-14 15:09:29 -0400

    update places to save models and give an option to train a new model if desired

--- src/TrainDoc2VecModel.py
+++ src/TrainDoc2VecModel.py
@@ -8,11 +8,11 @@ class TrainDoc2VecModel:
     def __init__(self):
         self.save_dir = "/home/dj/PycharmProjects/cs475/src/doc2vec_models/summer_2020/"
 
-    def get_model(self, train_corpus, save_path, vector_size, epochs):
+    def get_model(self, train_corpus, save_path, vector_size, epochs, train_new_model=False):
 
         model_path = self.save_dir + save_path
 
-        if os.path.exists(model_path): return gensim.models.doc2vec.Doc2Vec.load(model_path)
+        if os.path.exists(model_path) and not train_new_model: return gensim.models.doc2vec.Doc2Vec.load(model_path)
 
         model = gensim.models.doc2vec.Doc2Vec(vector_size=vector_size, min_count=2, epochs=epochs)
 
@@ -22,52 +22,52 @@ class TrainDoc2VecModel:
 
         return model
 
-    def get_aggregate_model(self, vector_size, epochs):
+    def get_aggregate_model(self, vector_size, epochs, train_new_model=False):
 
         corpus = ProjectCorpus()
         train_corpus = corpus.get_aggregate_corpus()
-        save_path = "aggregate/vs_{}_epochs_{}.model".format(vector_size, epochs)
+        save_path = "aggregate/models/vs_{}_epochs_{}.model".format(vector_size, epochs)
 
-        model = self.get_model(train_corpus, save_path, vector_size, epochs)
+        model = self.get_model(train_corpus, save_path, vector_size, epochs, train_new_model)
 
         return model
 
-    def get_mtg_model(self, vector_size, epochs):
+    def get_mtg_model(self, vector_size, epochs, train_new_model=False):
 
         corpus = ProjectCorpus()
         train_corpus = corpus.get_mtg_corpus()
-        save_path = "mtg/vs_{}_epochs_{}.model".format(vector_size, epochs)
+        save_path = "mtg/models/vs_{}_epochs_{}.model".format(vector_size, epochs)
 
-        model = self.get_model(train_corpus, save_path, vector_size, epochs)
+        model = self.get_model(train_corpus, save_path, vector_size, epochs, train_new_model)
 
         return model
 
-    def get_sports_model(self, vector_size, epochs):
+    def get_sports_model(self, vector_size, epochs, train_new_model=False):
 
         corpus = ProjectCorpus()
         train_corpus = corpus.get_sports_corpus()
-        save_path = "sports/vs_{}_epochs_{}.model".format(vector_size, epochs)
+        save_path = "sports/models/vs_{}_epochs_{}.model".format(vector_size, epochs)
 
-        model = self.get_model(train_corpus, save_path, vector_size, epochs)
+        model = self.get_model(train_corpus, save_path, vector_size, epochs, train_new_model)
 
         return model
 
-    def get_dance_model(self, vector_size, epochs):
+    def get_dance_model(self, vector_size, epochs, train_new_model=False):
 
         corpus = ProjectCorpus()
         train_corpus = corpus.get_dance_corpus()
-        save_path = "dance/vs_{}_epochs_{}.model".format(vector_size, epochs)
+        save_path = "dance/models/vs_{}_epochs_{}.model".format(vector_size, epochs)
 
-        model = self.get_model(train_corpus, save_path, vector_size, epochs)
+        model = self.get_model(train_corpus, save_path, vector_size, epochs, train_new_model)
 
         return model
 
-    def get_mtg_and_sports_model(self, vector_size, epochs):
+    def get_mtg_and_sports_model(self, vector_size, epochs, train_new_model=False):
 
         corpus = ProjectCorpus()
         train_corpus = corpus.get_mtg_and_sports_corpus()
-        save_path = "mtg_and_sports/vs_{}_epochs_{}.model".format(vector_size, epochs)
+        save_path = "mtg_and_sports/models/vs_{}_epochs_{}.model".format(vector_size, epochs)
 
-        model = self.get_model(train_corpus, save_path, vector_size, epochs)
+        model = self.get_model(train_corpus, save_path, vector_size, epochs, train_new_model)
 
         return model
